2aad3f9ea4f3ebd085f2907f2a0e56fd1415a9f7
Abbas Gholami <abbas.gholami71@gmail.com>
2026-08-21 09:08:07 +0200

    fix: read TXM image width and height from correct Cropped metadata fields

--- src/nomad_measurements_nxtomo/schema_packages/schema_package.py
+++ src/nomad_measurements_nxtomo/schema_packages/schema_package.py
@@ -365,12 +365,12 @@ class ELNZeissTXM(BaseNXtomoMeasurement, EntryData):
 
             # --- EXTRACT PREVIEW IMAGE FOR TXM ---
             img_width = 1010
-            width_data = txm_data.image_info.get('ImageWidth', {})
+            width_data = txm_data.recon_input_tomo_params.get('CroppedWidth', {})
             if isinstance(width_data, dict) and 'int32' in width_data:
                 img_width = width_data['int32']
 
             img_height = 1010
-            height_data = txm_data.image_info.get('ImageHeight', {})
+            height_data = txm_data.recon_input_tomo_params.get('CroppedHeight', {})
             if isinstance(height_data, dict) and 'int32' in height_data:
                 img_height = height_data['int32']
 
